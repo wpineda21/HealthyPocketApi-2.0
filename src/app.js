a533d2c7c89f3e27eb5851b5dccf04e8a996b4e9
William Pineda <00225919@uca.edu.sv>
2023-06-27 11:04:10 -0600

    add: Cors Configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors"; // Importa el paquete cors
+
 const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Configuración de CORS
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// Importa tus rutas
 import UserRoutes from "./routes/UserRoutes.js";
 import CitaRoutes from "./routes/CitaRoutes.js";
 import MedicoRoutes from "./routes/MedicoRoutes.js";
@@ -10,20 +21,14 @@ import RecetaRoutes from "./routes/RecetaRoutes.js"
 import MedicamentosRoutes from './routes/MedicamentoRoutes.js'
 import MenuRoutes from  './routes/MenuRoutes.js';
 
-// Middlewares
-app.use(morgan("dev"));
-app.use(express.json());
-
-app.use("/api/users",UserRoutes);
-app.use("/api/cita",CitaRoutes);
-app.use("/api/medico",MedicoRoutes);
-app.use("/api/examen",ExamenRoutes);
-app.use("/api/examen/detalle",DetallesRoutes);
-app.use("/api/receta",RecetaRoutes);
-app.use("/api/medicamento",MedicamentosRoutes);
-app.use("/api/Login",MenuRoutes)
-
-
-
+// Asigna las rutas
+app.use("/api/users", UserRoutes);
+app.use("/api/cita", CitaRoutes);
+app.use("/api/medico", MedicoRoutes);
+app.use("/api/examen", ExamenRoutes);
+app.use("/api/examen/detalle", DetallesRoutes);
+app.use("/api/receta", RecetaRoutes);
+app.use("/api/medicamento", MedicamentosRoutes);
+app.use("/api/Login", MenuRoutes);
 
 export default app;
